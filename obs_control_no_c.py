26c1399f2681e3299e4ce83efcb0ef973ad006c8
chansol824 <jcs6618@naver.com>
2024-06-13 17:29:39 +0900

    Update obs_control_no_c.py

--- obs_control_no_c.py
+++ obs_control_no_c.py
@@ -51,75 +51,39 @@ class OBS_Control:
         self.obsL = data.data
 
     def control(self):
-        if self.obsR and not self.obsL:  # 오른쪽 벽을 따라가지만 왼쪽 벽이 없음
-            speed = 0.3
-            if self.dist90R == 0:
-                speed = 0.0
-                steer = self.pre_steer
-                print('dist90R = 0')
-            else:
-                value = self.dist90R / self.dist75R
-                if value > 1:
-                    value = 1
-                elif value < -1:
-                    value = -1
+        if self.dist90L == 0.5:  # dist90L 값이 측정되지 않은 경우
+            steer = -1.0  # 좌회전 명령
+            speed = 0.2  # 속도 설정
+        elif self.dist90R == 0.5:  # dist90R 값이 측정되지 않은 경우
+            steer = 1.0  # 우회전 명령
+            speed = 0.2  # 속도 설정
+        else:
+            # dist90L 값 기반 PID 제어
+            error = self.dist90L - self.wall_dist  # 오차 계산
+            steer = self.kp * error  # PID 제어에 따른 조향각 계산
+            speed = 0.4  # 기본 속도 설정
 
-                theta = math.acos(value) * 180 / math.pi
-                print('theta:', theta)
-                steer = (15 - theta) * math.pi / 180 + self.kp * (self.wall_dist - self.dist90R)
-                print('steer:', steer)
-            print('following right wall')
-        elif self.obsR and self.obsL:  # 왼쪽 벽과 오른쪽 벽 모두 감지될 때
-            speed = 0.3
-            if self.dist90R == 0:
-                speed = 0.0
-                steer = self.pre_steer
-                print('dist90R = 0')
-            else:
-                value = self.dist90R / self.dist75R
-                if value > 1:
-                    value = 1
-                elif value < -1:
-                    value = -1
+            # 조향각 제한
+            if steer > 1.0:
+                steer = 1.0
+            elif steer < -1.0:
+                steer = -1.0
 
-                theta = math.acos(value) * 180 / math.pi
-                print('theta:', theta)
-                steer = (15 - theta) * math.pi / 180 + self.kp * (self.wall_dist - self.dist90R)
-                print('steer:', steer)
-            print('following right wall')
-        elif self.obsL:  # 왼쪽 벽을 따라가기
-            speed = 0.3
-            if self.dist90L == 0:
-                speed = 0.0
-                steer = self.pre_steer
-                print('dist90L = 0')
-            else:
-                value = self.dist90L / self.dist75L
-                if value > 1:
-                    value = 1
-                elif value < -1:
-                    value = -1
+        # Twist 메시지에 설정
+        self.cmd_msg.linear.x = speed  # 속도 설정
+        self.cmd_msg.angular.z = steer  # 조향각 설정
 
-                theta = math.acos(value) * 180 / math.pi
-                print('theta:', theta)
-                steer = (theta - 15) * math.pi / 180 + self.kp * (self.wall_dist - self.dist90L)
-                print('steer:', steer)
-            print('following left wall')
-        else:  # 양쪽 벽이 없을 때 정지
-            speed = 0.3
-            steer = 0
-            print('no conditions met, stopping')
-
-        self.cmd_msg.linear.x = speed
-        self.cmd_msg.angular.z = steer
+        # 제어 메시지 발행
         self.pub.publish(self.cmd_msg)
-        self.pre_steer = steer
+
+    def run(self):
+        while not rospy.is_shutdown():
+            self.control()
+            self.rate.sleep()
 
 def main():
     OBS_driving = OBS_Control()
-    while not rospy.is_shutdown():
-        OBS_driving.control()
-        OBS_driving.rate.sleep()
+    OBS_driving.run()
 
 if __name__ == "__main__":
     main()
