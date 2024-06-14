3e3b9f98efd65317a7c7c22fe7c5a0e950031e59
chansol824 <jcs6618@naver.com>
2024-06-15 01:41:39 +0900

    Update obs_control_no_c.py

--- obs_control_no_c.py
+++ obs_control_no_c.py
@@ -23,10 +23,10 @@ class OBS_Control:
         self.obsL = False
         self.dist90R = 1
         self.dist90L = 1
-        self.dist75R = 0.5
+        self.dist75R = 0.75
         self.dist75L = 1
-        self.wall_dist = 0.3
-        self.kp = 1.5
+        self.wall_dist = 0.25
+        self.kp = 0.4
         self.pre_steer = 0
 
         self.rate = rospy.Rate(10)
@@ -54,42 +54,66 @@ class OBS_Control:
         speed = 0.3  # 기본 속도
 
         # 센서값이 모두 측정되지 않은 경우
-        if self.dist90L == 1 and self.dist75L == 1 and self.dist90R == 1 and self.dist75R == 0.5:
-            steer = -1.5  # 방향을 유지
+        if self.dist90L == 1 and self.dist75L == 1 and self.dist75R == 0.75:
+            steer = 1.5  # 방향을 유지
             speed = 0.3  # 속도를 낮춤
-            print("Nothing")
+            print("None")
         
         # 우측 센서값이 측정되지 않은 경우
-        elif self.dist90R == 1 and self.dist75R == 0.5 and not (self.dist90L and self.dist75L) == 1:
+        elif self.dist75R == 0.75 and not self.dist90L == 1 and not self.dist75L == 1:
             steer = -1.5  # 우회전 명령
-            speed = 0.3  # 속도 설정
+            speed = 0.5  # 속도 설정
             print("turn RIGHT")
 
         # 좌측 센서값이 측정되지 않은 경우
-        elif self.dist90L == 1 and self.dist75L == 1 and not self.dist90R == 1 and not self.dist75R == 0.5:
-            steer = 1.  # 좌회전 명령
-            speed = 0.3  # 속도 설정
+        elif self.dist90L == 1 and self.dist75L == 1 and not self.dist75R == 0.75:
+            steer = 1.5  # 좌회전 명령
+            speed = 0.2  # 속도 설정
             print("turn LEFT")
         
         # 왼쪽 센서가 측정되는 경우 PID 제어
-        elif 0.35 < self.dist90L < 1.1 and 0.35 < self.dist75L < 1.1:
-            error = (self.dist75L + self.dist90L) / 2 - self.wall_dist  # 오차 계산
+        elif 0.35 < self.dist90L < 1 and 0.35 < self.dist75L < 1.1:
+            error = (self.dist75R + self.dist90L) / 2 + self.wall_dist # 오차 계산
             steer = self.kp * error  # PID 제어에 따른 조향각 계산
-            speed = 0.5  # 기본 속도 설정
-            print("Left sensor", steer)
-        
-        # 왼쪽 센서가 벽에 매우 가까운 경우
-        elif 0 < self.dist90L < 0.34 and 0 < self.dist75L < 0.34:
-            error = -(self.dist75L + self.dist90L) / 2 - self.wall_dist  # 오차 계산
-            steer = self.kp * error  # PID 제어에 따른 조향각 계산
-            speed = 0.5  # 기본 속도 설정
-            print("Ringt sesor", steer)
+            speed = 0.45  # 기본 속도 설정
+            print("Right wall follow", steer)
 
         # 조향각 제한
         if steer > 1.0:
             steer = 1.0
         elif steer < -1.0:
             steer = -1.0
+        
+        # 왼쪽 센서가 벽에 매우 가까운 경우
+        elif 0 < self.dist90L < 0.38 and 0 < self.dist75L < 0.34:
+            error = -(self.dist75L + self.dist90L) / 2 + self.wall_dist # 오차 계산
+            steer = self.kp * error  # PID 제어에 따른 조향각 계산
+            speed = 0.45  # 기본 속도 설정
+            print("Left wall follow", steer)
+
+        # 조향각 제한
+        if steer > 1.0:
+            steer = 1.0
+        elif steer < -1.0:
+            steer = -1.0
+
+        # dist75L만 인식되는 경우 오른쪽으로 조향
+        elif self.dist75L < 0.25 and self.dist90L == 1 and self.dist75R == 0.75:
+            steer = -0.75  # 우회전 명령
+            speed = 0.2  # 기본 속도 설정
+            print("Only dist75L")
+
+        # dist90L만 인식되는 경우 속도 줄임
+        elif self.dist90L != 1 and self.dist75L == 1 and self.dist75R == 0.75:
+            steer = -1.5  # 조향 없음
+            speed = 0.3  # 속도 줄임
+            print("Only dist90L")
+
+        # dist90L와 dist75R만 인식되는 경우 왼쪽으로 조향
+        elif self.dist90L != 1 and self.dist75L == 1 and self.dist75R != 0.75:
+            steer = 1.5  # 좌회전 명령
+            speed = 0.3  # 기본 속도 설정
+            print("dist90L and dist75R")
 
         # Twist 메시지에 설정
         self.cmd_msg.linear.x = speed  # 속도 설정
